8d5ffc6c844ea1f0c29536a79b6468fe683e2bdb
Mauro <Mauro@MacBook-Air-de-Rita.local>
2022-06-20 16:45:51 -0300

    Creacion de archivos y testeos

---
+++ extract_data.xq
@@ -0,0 +1,5 @@
+<artist_data xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation= "artist_data.xsd">
+
+<name>{data(doc("artist_info_TEST.xml")/metadata/artist/name)}</year>
+
+</artist_data>
--- tpe.sh
+++ tpe.sh
@@ -7,6 +7,7 @@ curl -o artist_info.xml https://musicbrainz.org/ws/2/artist/${ARTIST_ID}?inc=wor
 curl -o recordings_info.xml https://musicbrainz.org/ws/2/recording?query=arid:${ARTIST_ID}&limit=1000
 
 
-
-
-
+echo '<collection>
+    <doc href="./files_info/recordings_info.xml" />
+    <doc href="./files_info/artist_info.xml" />
+</collection>' > collection_dir.xml
