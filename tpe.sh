87f7102873dc16867dcca0c55bf8db0bcdae842b
maurovella <mvella@itba.edu.ar>
2022-06-21 17:16:17 -0300

    Progreso en xquery y bash

--- tpe.sh
+++ tpe.sh
@@ -2,12 +2,18 @@
 
 ARTIST_ID=$1
 
-curl -o artist_info.xml https://musicbrainz.org/ws/2/artist/${ARTIST_ID}?inc=works
+curl -o artist_info_aux.xml https://musicbrainz.org/ws/2/artist/${ARTIST_ID}?inc=works
 
-curl -o recordings_info.xml https://musicbrainz.org/ws/2/recording?query=arid:${ARTIST_ID}&limit=1000
+curl -o recordings_info_aux.xml https://musicbrainz.org/ws/2/recording?query=arid:${ARTIST_ID}&limit=1000
 
+sleep 1
 
-echo '<collection>
-    <doc href="./files_info/recordings_info.xml" />
-    <doc href="./files_info/artist_info.xml" />
-</collection>' > collection_dir.xml
+sed 's/<metadata xmlns="http:\/\/musicbrainz\.org\/ns\/mmd-2\.0#">//g;s/<\/metadata>//g' artist_info_aux.xml > artist_info.xml
+
+sleep 1
+
+sed 's/<metadata.*xmlns:ns2="http:\/\/musicbrainz\.org\/ns\/ext#-2\.0">//g;s/<\/metadata>//g;s/ns2:score="100"//g' recordings_info_aux.xml > recordings_info.xml
+
+# no funcionan los remove ...
+rm -rf artist_info_trash
+rm -rf recordings_info_trash
